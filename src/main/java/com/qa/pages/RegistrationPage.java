dd572ef3e4499d1869f2189a726bd3bab8f8e5ad
manirathnam800 <manirathnam800@gmail.com>
2021-09-26 11:40:42 +0530

    added registration file

---
+++ src/main/java/com/qa/pages/RegistrationPage.java
@@ -0,0 +1,16 @@
+package com.qa.pages;
+
+public class RegistrationPage {
+
+	
+	
+	public RegistrationPage() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	public void doRegistration() {
+		System.out.println("Registration successfull");
+	}
+}
