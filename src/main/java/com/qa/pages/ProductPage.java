c6812455a445611676930dab449d1adec227a464
manirathnam800 <manirathnam800@gmail.com>
2022-09-08 16:28:16 +0530

    added products page

---
+++ src/main/java/com/qa/pages/ProductPage.java
@@ -0,0 +1,14 @@
+package com.qa.pages;
+
+public class ProductPage {
+	
+	
+	private String productName;
+	
+	private int proId;
+	
+	public ProductPage() {
+		// TODO Auto-generated constructor stub
+	}
+  }
+}	
